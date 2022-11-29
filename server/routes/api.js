eab423e6a8e4196532c26c372bb721f055d716da
Dan Pietsch <drpietsch14@gmail.com>
2022-11-29 18:50:51 -0500

    completed code review cleanup and comment add

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,7 +7,9 @@ router.post(
   '/',
   apiController.basicTestRunner,
   apiController.getTranslation,
-  (req, res, next) => {
+  // add middleware to save query to db if user is logged in (requestController.saveRequest)
+  // could add middleware to get requests/update the history here
+  (req, res) => {
     res
       .setHeader('Access-Control-Allow-Origin', '*')
       .status(200)
