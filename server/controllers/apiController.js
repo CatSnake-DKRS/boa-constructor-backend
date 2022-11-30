ab900fcad24c5eaa3c2bac231aa35e1a266bf421
Robbie <RobertIGottlieb@gmail.com>
2022-11-29 20:16:41 -0500

    adding routing

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -15,7 +15,7 @@ apiController.basicTestRunner = (req, res, next) => {
 };
 
 // middleware for translating code into plain english
-apiController.getTranslation = async (req, res, next) => {
+apiController.englishToCode = async (req, res, next) => {
   console.log(req.body);
   // get the data from the body on the request
   const { language } = req.body; // Python, JavaScript...
@@ -46,6 +46,97 @@ apiController.getTranslation = async (req, res, next) => {
 
     // storing text in form of the string on the response locals object
     res.locals.text = responseText.join('');
+  } catch (err) {
+    // error handling
+    const ourErr = {
+      log: 'Express error handler caught error in the getTranslation apiController',
+    };
+    next(ourErr);
+  }
+
+  next();
+};
+
+apiController.codeToEnglish = async (req, res, next) => {
+  console.log(req.body);
+  // get the data from the body on the request
+  const { language } = req.body; // Python, JavaScript...
+  const { text } = req.body; // code...
+
+  // tempreture can a stretch feature - if we let user decide on the tempreture,
+  // we will get on the req.body as well
+  const tempreture = 0.7;
+
+  try {
+    // making a call to the Dall-e API
+    const response = await openai.createCompletion({
+      model: 'text-davinci-002',
+      // prompt: `Summarize this for a second-grade student:\n${language} \n${text}`,
+      prompt: `Summarize this for a second-grade student:\n${text}`,
+      temperature: tempreture,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    // getting rid of the first 2 '\n' symbols (they will probably be on all of our responses)
+    const responseText = response.data.choices[0].text.split('');
+    while (responseText[0] === '\n' && responseText.length > 1) {
+      responseText.shift();
+    }
+
+    // storing text in form of the string on the response locals object
+    res.locals.text = responseText.join('');
+  } catch (err) {
+    // error handling
+    const ourErr = {
+      log: 'Express error handler caught error in the getTranslation apiController',
+    };
+    next(ourErr);
+  }
+
+  next();
+};
+
+apiController.englishToSql = async (req, res, next) => {
+  console.log(req.body);
+  // get the data from the body on the request
+  const { text, schema } = req.body; // code...
+
+  // tempreture can a stretch feature - if we let user decide on the tempreture,
+  // we will get on the req.body as well
+  const temperature = 0.7;
+  const schemString = '';
+  // schema = { Entertainer: 'id, name, salary, movie_id' };
+
+  for (const table in schema) {
+    const tableString = table + '(' + schema[table] + `)\n# `;
+    schemaString += tableString;
+  }
+  // text =
+  //   'Select the list of entertainers who made more than 5 million dollars in 2011';
+
+  try {
+    // making a call to the Dall-e API
+    const response = await openai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: `### Postgres SQL tables, with their properties:${schemaString}\n### ${text}\n SELECT`,
+      temperature: tempreture,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    // getting rid of the first 2 '\n' symbols (they will probably be on all of our responses)
+    const responseText = response.data.choices[0].text.split('');
+    while (responseText[0] === '\n' && responseText.length > 1) {
+      responseText.shift();
+    }
+
+    // storing text in form of the string on the response locals object
+    res.locals.text = 'SELECT' + responseText.join('');
   } catch (err) {
     // error handling
     const ourErr = {
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,33 @@ const apiController = require('../controllers/apiController');
 const router = express.Router();
 
 router.post(
-  '/',
+  '/entocode',
   apiController.basicTestRunner,
-  apiController.getTranslation,
+  apiController.englishToCode,
+  (req, res, next) => {
+    res
+      .setHeader('Access-Control-Allow-Origin', '*')
+      .status(200)
+      .json(res.locals.text);
+  }
+);
+
+router.post(
+  '/codetoen',
+  apiController.basicTestRunner,
+  apiController.codeToEnglish,
+  (req, res, next) => {
+    res
+      .setHeader('Access-Control-Allow-Origin', '*')
+      .status(200)
+      .json(res.locals.text);
+  }
+);
+
+router.post(
+  '/entosql',
+  apiController.basicTestRunner,
+  apiController.englishToSql,
   (req, res, next) => {
     res
       .setHeader('Access-Control-Allow-Origin', '*')
