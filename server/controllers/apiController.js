48be461faa9855356918417240b2f27ac92407cd
Robbie <RobertIGottlieb@gmail.com>
2022-11-30 16:21:45 -0500

    completed database integration for user storage + added bcrypt hashing

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,3 +1,4 @@
+const { teardown } = require('mocha');
 const { Configuration, OpenAIApi } = require('openai');
 
 // setting up authorized access to the Dall-e API
@@ -6,138 +7,82 @@ const configuration = new Configuration({
   apiKey: process.env.DALLE_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
 const apiController = {};
+const manageRequest = async (req, res, next, prompt, temperature, schema) => {
+  // get the query from data on request
+  let { query } = req.body;
 
-apiController.basicTestRunner = (req, res, next) => {
-  if (req.body.language === '__test') res.status(200).send(req.body.text);
-  else next();
-};
-
-// middleware for translating code into plain english
-apiController.englishToCode = async (req, res, next) => {
-  // get the data from the body on the request
-  const { text } = req.body; // code...
-
-  // tempreture can a stretch feature - if we let user decide on the tempreture,
-  // we will get on the req.body as well
-  const temperature = 0.7;
-
+  query += schema ? ' \n SELECT' : '';
   try {
     // making a call to the Dall-e API
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `Use javascript to ${text} `,
-      temperature: 0,
+      model: 'text-davinci-002',
+      prompt: `${prompt} ${query}`,
+      temperature,
       max_tokens: 100,
       top_p: 1,
       frequency_penalty: 0.2,
       presence_penalty: 0,
     });
-
     // getting rid of the first 2 '\n' symbols (they will probably be on all of our responses)
-    const responseText = response.data.choices[0].text.split('');
-    while (responseText[0] === '\n' && responseText.length > 1) {
-      responseText.shift();
+    const responseTranslation = response.data.choices[0].text.split('');
+    while (responseTranslation[0] === '\n' && responseTranslation.length > 1) {
+      responseTranslation.shift();
     }
-
-    // storing text in form of the string on the response locals object
-    res.locals.text = responseText.join('');
+    // storing translation in form of the string on the response locals object
+    res.locals.query = query;
+    res.locals.translation = `${
+      schema ? 'SELECT' : ''
+    }${responseTranslation.join('')}`;
+    res.locals.schema = schema;
   } catch (err) {
     // error handling
     const ourErr = {
-      log: 'Express error handler caught error in the getTranslation apiController',
+      log: 'Express error handler caught error in the englishToCode apiController',
     };
     next(ourErr);
   }
+  return next();
+};
 
-  next();
+apiController.basicTestRunner = (req, res, next) => {
+  if (req.body.language === '__test') res.status(200).send(req.body.query);
+  else next();
+};
+
+// middleware for translating code into plain english
+apiController.englishToCode = async (req, res, next) => {
+  await manageRequest(req, res, next, 'Use javascript to', 0);
 };
 
 apiController.codeToEnglish = async (req, res, next) => {
-  console.log(req.body);
-  // get the data from the body on the request
-  const { language } = req.body; // Python, JavaScript...
-  const { text } = req.body; // code...
-
-  // tempreture can a stretch feature - if we let user decide on the tempreture,
-  // we will get on the req.body as well
-  const temperature = 0.7;
-
-  try {
-    // making a call to the Dall-e API
-    const response = await openai.createCompletion({
-      model: 'text-davinci-002',
-      // prompt: `Summarize this for a second-grade student:\n${language} \n${text}`,
-      prompt: `Summarize this for a second-grade student:\n${text}`,
-      temperature,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-
-    // getting rid of the first 2 '\n' symbols (they will probably be on all of our responses)
-    const responseText = response.data.choices[0].text.split('');
-    while (responseText[0] === '\n' && responseText.length > 1) {
-      responseText.shift();
-    }
-
-    // storing text in form of the string on the response locals object
-    res.locals.text = responseText.join('');
-  } catch (err) {
-    // error handling
-    const ourErr = {
-      log: 'Express error handler caught error in the getTranslation apiController',
-    };
-    next(ourErr);
-  }
-
-  next();
+  await manageRequest(
+    req,
+    res,
+    next,
+    'Summarize this for a second-grade student:\n',
+    0
+  );
 };
 
 apiController.englishToSql = async (req, res, next) => {
-  // get the data from the body on the request
-  const { text, schema } = req.body; // code...
-  // tempreture can a stretch feature - if we let user decide on the tempreture,
-  // we will get on the req.body as well
-  const temperature = 0.7;
+  const { schema } = req.body; // code...
   let schemaString = '';
 
   for (const table in schema) {
-    const tableString = '#' + table + '(' + schema[table] + `)\n`;
+    const tableString = `#${table}(${schema[table]})\n`;
     schemaString += tableString;
   }
 
-  try {
-    // making a call to the Dall-e API
-    const response = await openai.createCompletion({
-      model: 'text-davinci-002',
-      prompt: `### Postgres SQL tables, with their properties:\n#\n ${schemaString}\n### ${text}\n SELECT`,
-      temperature: temperature,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-
-    // getting rid of the first 2 '\n' symbols (they will probably be on all of our responses)
-    const responseText = response.data.choices[0].text.split('');
-    while (responseText[0] === '\n' && responseText.length > 1) {
-      responseText.shift();
-    }
-
-    // storing text in form of the string on the response locals object
-    res.locals.text = 'SELECT' + responseText.join('');
-  } catch (err) {
-    // error handling
-    const ourErr = {
-      log: 'Express error handler caught error in the englishToSql apiController',
-    };
-    next(ourErr);
-  }
-
-  next();
+  console.log('schemastring', schemaString);
+  await manageRequest(
+    req,
+    res,
+    next,
+    '### Postgres SQL tables, with their properties:\n#\n',
+    0,
+    schemaString
+  );
 };
 
 module.exports = apiController;
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,34 +1,64 @@
 const { Sequelize, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = new Sequelize(process.env.URI);
 
-// const sequelize = new Sequelize('localdev', 'localdev', 'localdev', {
-//   host: 'localhost',
-//   port: process.env.DB_PORT,
-//   dialect: 'postgres',
-// });
-
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-});
+  {
+    //adding hooks to hash password upon save and to check for password matching
+    hooks: {
+      //before file is created the password will be saved to DB as hashed password
+      beforeCreate: async (user) => {
+        if (user.password) {
+          //ten round of hashing
+          const salt = await bcrypt.genSaltSync(10);
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      },
+      //before file is updated the password will be saved to DB as hashed password
+      beforeUpdate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSaltSync(10);
+          user.password = bcrypt.hashSync(user.password, salt);
+        }
+      },
+    },
+    //method that will be attached to each instance to check if user entered, plain-text password
+    //matches the hashed version stored in DB
+    instanceMethods: {
+      validPassword: (password) => {
+        return bcrypt.compareSync(password, this.password);
+      },
+    },
+  }
+);
+// adds validpassword function to the prototype of User instance
+//this allows us to use the method on individual instances
+//of user (for example, those returned by findOne method)
+User.prototype.validPassword = async (password, hashedPassInDB) => {
+  return await bcrypt.compareSync(password, hashedPassInDB);
+};
 
 const Request = sequelize.define('Request', {
   id: {
@@ -54,17 +84,7 @@ Request.belongsTo(User);
 
 sequelize.sync();
 
-async function auth() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-
 module.exports = {
-  auth,
   User,
   Request,
   sequelize,
