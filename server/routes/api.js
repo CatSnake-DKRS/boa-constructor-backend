d695b89bc71d35e9168ff7cc14b505b3f22c5942
Stephen <stephen.anthony.rivas@gmail.com>
2022-12-01 15:13:56 -0500

    api updated

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const apiController = require("../controllers/apiController");
-const requestController = require("../controllers/requestController");
+const express = require('express');
+const apiController = require('../controllers/apiController');
+const requestController = require('../controllers/requestController');
 
 const router = express.Router();
 
 router.post(
-  "/codetoen",
+  '/codetoen',
   apiController.basicTestRunner,
   apiController.codeToEnglish,
   requestController.saveRequest,
   (req, res) => {
     res
-      .setHeader("Access-Control-Allow-Origin", "*")
+      .setHeader('Access-Control-Allow-Origin', '*')
       .status(200)
       .json(res.locals);
   }
 );
 
 router.post(
-  "/entocode",
+  '/entocode',
   apiController.basicTestRunner,
   apiController.englishToCode,
   requestController.saveRequest,
   (req, res) => {
     console.log(res.locals);
     res
-      .setHeader("Access-Control-Allow-Origin", "*")
+      .setHeader('Access-Control-Allow-Origin', '*')
       .status(200)
       .json(res.locals);
   }
 );
 
 router.post(
-  "/entosql",
+  '/entosql',
   apiController.basicTestRunner,
   apiController.englishToSql,
   requestController.saveRequest,
   (req, res) => {
     res
-      .setHeader("Access-Control-Allow-Origin", "*")
+      .setHeader('Access-Control-Allow-Origin', '*')
       .status(200)
       .json(res.locals);
   }
